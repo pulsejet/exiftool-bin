93439b17c49492b1669f653c8dbc219d06b33ff2
Varun Patil <varunpatil@ucla.edu>
2023-11-21 12:17:19 -0800

    Remove libssl headers

--- build-alpine.sh
+++ build-alpine.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 apk update
-apk add bash curl wget make gcc perl perl-utils perl-dev unzip zip musl-dev openssl-dev
+apk add bash curl wget make gcc perl perl-utils perl-dev unzip zip musl-dev
 bash build.sh
