c31226bb329af2eaa67e813b732cb860e99e3e36
Varun Patil <varunpatil@ucla.edu>
2023-11-21 12:04:01 -0800

    add openssl headers

--- build-alpine.sh
+++ build-alpine.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 apk update
-apk add bash curl wget make gcc perl perl-utils perl-dev unzip zip musl-dev
+apk add bash curl wget make gcc perl perl-utils perl-dev unzip zip musl-dev openssl-dev
 bash build.sh
--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y make gcc libperl-dev wget curl sudo unzip zip
+apt-get install -y make gcc libperl-dev wget curl sudo unzip zip libssl-dev
 bash build.sh
