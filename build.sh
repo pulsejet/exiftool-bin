a88e156f654f448160908442174b47f6c2ad9571
Varun Patil <varunpatil@ucla.edu>
2023-11-21 11:37:15 -0800

    12.70

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cpanm Archive::Zip PAR PAR::Packer IO::String
 zip -y -q -r cpanm-log.zip ~/.cpanm/
 
 # Get repo
-wget -O exiftool.zip https://github.com/exiftool/exiftool/archive/refs/tags/12.60.zip
+wget -O exiftool.zip https://github.com/exiftool/exiftool/archive/refs/tags/12.70.zip
 unzip -q exiftool.zip
 rm exiftool.zip
 cd exiftool-*
